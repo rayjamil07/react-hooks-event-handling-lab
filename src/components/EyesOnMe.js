2bd8d665b8947a27b82828dcf9e15028a5f9452e
rayjamil <ryanwekesa019@gmail.com>
2025-01-23 17:22:18 +0300

    Adds multiple files

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesonMe(){
+    function Focus(event){
+        console.log("Good!")
+    }
+    function Blur(event){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <div>
+            <button onFocus={Focus} onBlur={Blur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesonMe;
